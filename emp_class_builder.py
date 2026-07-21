5bd78de220dff31fb78d1cc9db77768c6f89974f
Abdussamad-27 <abdussamad27ab@gmail.com>
2026-07-21 15:43:48 +0530

    task1 added

--- emp_class_builder.py
+++ emp_class_builder.py
@@ -1,15 +1,23 @@
 class employee:
-     def display(self,emp_id,emp_name,emp_deprt):
-          self.emp_id=emp_id
-          self.emp_name=emp_name
-          self.emp_deprt=emp_deprt
+     pass
 
-          print(f"Emp_Id = {self.emp_id} ")
-          print(f"Name = {self.emp_name} ")
-          print(f"Department = {self.emp_deprt} ")
-          print("-"*20)
+          
 
 e1=employee()
 e2=employee()
-e1.display("U15OP","Ram","HR")
-e2.display("U16LK","Rahul","Testing")
+e1.emp_id="U16LK"
+e1.emp_name="Rahul"
+e1.emp_deprt="HR"
+
+e2.emp_id="U16LK"
+e2.emp_name="Ram"
+e2.emp_deprt="Testing"
+print(f"Emp_Id = {e1.emp_id} ")
+print(f"Name = {e1.emp_name} ")
+print(f"Department = {e1.emp_deprt} ")
+print("-"*20)
+
+print(f"Emp_Id = {e2.emp_id} ")
+print(f"Name = {e2.emp_name} ")
+print(f"Department = {e2.emp_deprt} ")
+print("-"*20)
