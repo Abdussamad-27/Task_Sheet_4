7fadc35f40310a9116c6f1708a0bfe9e29be2750
Abdussamad-27 <abdussamad27ab@gmail.com>
2026-07-23 15:15:15 +0530

    task 7 importing module added

---
+++ hr_utils.py
@@ -0,0 +1,12 @@
+Task 8:
+hr_utils.py
+def generate_emp_id(dept_code, number):
+    return f"{dept_code}-{number:03d}"
+
+def is_eligible_for_bonus(years, rating):
+    return years >= 2 and rating >= 4.0
+
+def calculate_attendance_percent(present_days, total_days):
+    return (present_days / total_days) * 100
+
+Main1.py
