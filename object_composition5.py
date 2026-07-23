d476f05786de06571ffc4a5714bb83988df6db29
Abdussamad-27 <abdussamad27ab@gmail.com>
2026-07-23 14:58:49 +0530

    task 5 added

--- object_composition5.py
+++ object_composition5.py
@@ -1,17 +1,28 @@
-class department:
-    def __init__(self,dept,list):
-        self.dept=dept
-        self.list=list
+class Employee:
+    pass
 
-    def list_emp(self):
-        print("name : ",self.list,"Department : ",self.dept)
-list=[]
-dept=department("BCA",list)
-e1=department("",list)
-e2=department("BCA",list)
-e3=department("BCA",list)
-e1.list.append("Rahul")
-e1.list.append("Raju")
-e1.list.append("Ram")
-dept.list_emp()
 
+class Department:
+    dept_name = ""
+    employees = []  
+    def list_employees(self):
+        print(f"Employees in {self.dept_name}:")
+        for emp in self.employees:
+            print(f"- {emp.name}")
+
+
+emp1 = Employee() 
+emp1.name = "Aditya"
+emp2= Employee()
+emp2.name="Rakshita"
+emp3= Employee() 
+emp3.name="Abdusammad"
+dept = Department()
+dept.dept_name = "Engineering"
+
+
+dept.employees.append(emp1)
+dept.employees.append(emp2)
+dept.employees.append(emp3)
+
+dept.list_employees()
